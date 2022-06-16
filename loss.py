6a811ab8bdecdbeb7ff84421aad32b4953f9af06
MatiasSepulvedaGodoy <msepulveda@instacrops.com>
2022-06-16 17:47:02 +0200

    We change for our original dice loss.

--- loss.py
+++ loss.py
@@ -2,25 +2,34 @@
 import torch
 
 class DiceLoss(torch.nn.Module):
-  def __init__(self, smooth=1., n_classes=3) -> None:
-    super(DiceLoss, self).__init__()
-    self.smooth = smooth
-    self.n_classes = n_classes
 
-  def compute_dice(self, output, target):
-    loss = 1. - ((2. * (output.contiguous().view(-1)*target.contiguous().view(-1)).sum() + 
-                self.smooth) /(output.contiguous().view(-1).sum() + target.contiguous().view(-1).sum() + self.smooth))
-    return loss
+   def __init__(self, n_classes=3, smooth = 1.) -> None:
+       super(DiceLoss, self).__init__()
+       self.n_classes = n_classes
+       self.smooth = smooth
 
-  def forward(self, output, target):
-    
-    dice_per_class = []
-    dice_sum = 0.
-    for i in range(0, self.n_classes):
-      dice = self.compute_dice(output[:, i, :, :], target[:, i, :, :])
-      dice_per_class.append(dice)
-      dice_sum += dice
+   def compute_dice(self, outputs, targets):
+     targets = targets.float()
+     smooth = 1e-5
+     intersect = torch.sum(torch.mul(outputs, targets))
+     y_sum = torch.sum(torch.mul(targets, targets))
+     z_sum = torch.sum(torch.mul(outputs, outputs))
 
-    losses = {'avg_loss': dice_sum/self.n_classes, 'background_loss': dice_per_class[0], 
-              "epicardium_loss": dice_per_class[1], "endocardium_loss": dice_per_class[2]}
-    return losses
+     loss = (2. * intersect + smooth) / (z_sum + y_sum + smooth)
+     loss = 1. - loss
+     return loss
+
+   def forward(self, outputs, targets):
+     
+     class_wise_dice = []
+     dice_loss = 0.
+
+     for i in range(0, self.n_classes):
+       dice = self.compute_dice(outputs[:, i, :, :], targets[:, i, :, :])
+       class_wise_dice.append(dice.detach())
+       dice_loss += dice 
+
+     losses = {'avg_loss': dice_loss/self.n_classes, 'background_loss': class_wise_dice[0], 
+               "epicardium_loss": class_wise_dice[1], "endocardium_loss": class_wise_dice[2]}
+
+     return losses
