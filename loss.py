867af53d964fb1d99b0bffc6f548db7a809df53b
MatiasSepulvedaGodoy <msepulveda@instacrops.com>
2022-06-16 17:32:57 +0200

    We fix a problem in the loss.

--- loss.py
+++ loss.py
@@ -8,11 +8,8 @@ class DiceLoss(torch.nn.Module):
     self.n_classes = n_classes
 
   def compute_dice(self, output, target):
-    output_flat = output.contiguous().view(-1)
-    target_flat = target.contiguous().view(-1)
-    intersection = (output_flat*target_flat).sum()
-
-    loss = 1. - ((2. * intersection + self.smooth) /(output_flat.sum() + target_flat.sum() + self.smooth))
+    loss = 1. - ((2. * (output.contiguous().view(-1)*target.contiguous().view(-1)).sum() + 
+                self.smooth) /(output.contiguous().view(-1).sum() + target.contiguous().view(-1).sum() + self.smooth))
     return loss
 
   def forward(self, output, target):
